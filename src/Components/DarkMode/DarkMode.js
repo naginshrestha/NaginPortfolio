078aa808c3dd109b4370245fb3494dff46a2c0e2
naginshrestha <72690596+naginshrestha@users.noreply.github.com>
2023-04-05 21:25:25 +1000

    bugs fixing

--- src/Components/DarkMode/DarkMode.js
+++ src/Components/DarkMode/DarkMode.js
@@ -4,9 +4,9 @@ import '../DarkMode/DarkMode'
 const DarkMode = () => {
   return (
     <>
-      <input type="checkbox" id="darkMode" className="dark-mode-checkbox" />
+      {/* <input type="checkbox" id="darkMode" className="dark-mode-checkbox" />
       <label htmlFor="darkMode">
-    </label>
+    </label> */}
     </>
   )
 }
